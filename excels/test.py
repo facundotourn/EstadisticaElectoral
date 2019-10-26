a3ef5d2a2e52559f0f40022ce7fc3e754b8800c7
Martin <martin.serramo@gmail.com>
2019-10-26 16:16:50 -0300

    sdlsdsd

---
+++ excels/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 16:13:22 2019
+
+@author: marti
+"""
+import pandas as pd
+
+milnnc = pd.read_excel("resultados_2015_nacionales_presidente_y_vicepresidente.xlsx")
