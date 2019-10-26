9df807110615657d6ae7ac9975a5ee499cb7db7b
Martin <martin.serramo@gmail.com>
2019-10-26 18:14:49 -0300

    dale

--- script-electoral.py
+++ script-electoral.py
@@ -1,20 +1,29 @@
 import pandas as pd
-
-try:
-    g = pd.read_excel(".\\excels\\resultados_1983_-_presidente_y_vicepresidente_667.xlsx")
-except:
-    print("No existe ese archivo")
-
+    
+    
 anios = ["1993", "1995", "1997", "1999", "2001", "2003", "2005", "2007", "2009", "2011", "2015", "2017", "2019"]
-instancias = ["Paso", "Primera vuelta", "Segunda vuelta"]
+instancias = ["PASO", "Primera vuelta", "Segunda vuelta"]
 tipos = ["Diputados", "Presidente" ]#, "Senadores"]
+
+            
+def procesarArchivo(data, anio, instancia, tipo, path):
+    hojas = pd.ExcelFile(path).sheet_names
+    print(hojas)
+    for hoja in hojas
+    
+    return;
 
 for anio in anios:
     for instancia in instancias:
         for tipo in tipos:
-            print(".\\" + instancia + "\\" + tipo + "\\" + anio + ".xls")
+            print(".\\excels\\" + instancia + "\\" + tipo + "\\" + anio + ".xlsx")
             try:
-                g = pd.read_excel(".\\" + instancia * "\\" + tipo + "\\" + anio + ".xls")
-            except:
+                path = ".\\excels\\" + instancia + "\\" + tipo + "\\" + anio + ".xlsx"
+                data = pd.read_excel(path)
+                procesarArchivo(data,anio,instancia,tipo,path)
+                print("Detectado")
+            except FileNotFoundError:
                 print("No hubo " + instancia + " de " + tipo + " en el año " + anio)
             print()
+            
+            
