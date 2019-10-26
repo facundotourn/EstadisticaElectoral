a98ed353dc408ea590a6c6c6c2f76c1baaa452ae
Martin <martin.serramo@gmail.com>
2019-10-26 17:56:54 -0300

    ss

--- excels/test.py
+++ excels/test.py
@@ -6,4 +6,6 @@ Created on Sat Oct 26 16:13:22 2019
 """
 import pandas as pd
 
-milnnc = pd.read_excel("resultados_2015_nacionales_presidente_y_vicepresidente.xlsx")
+ba = pd.read_excel("resultados_2015_nacionales_presidente_y_vicepresidente.xlsx", "Buenos Aires")
+
+ba.columns = ["col1","col2","col3","col4" ]
